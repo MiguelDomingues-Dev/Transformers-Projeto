be1609773a9d7dc0dbeb31b705adf5fd9e34f92a
MiguelDomingues-Dev <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2023-11-01 11:09:00 -0300

    Criação da branch

---
+++ html/index.js
@@ -0,0 +1,4 @@
+function click() {
+    var res = document.getElementsByClassName('name')
+    res.innerHTML = ("Olá")
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+function midyaQuerys() {
+    if (window.innerWidth >= 768) {
+    nav.style.display = 'block'
+} else {
+    nav.style.display = 'none'
+}
+}
+
+function clickMenu(){
+        if (nav.style.display == 'none') {
+            nav.style.display = 'block'
+    } else {
+        nav.style.display = 'none'
+    }
+}
+
+
